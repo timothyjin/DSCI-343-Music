ee74591d519b6d2cb77ab2af1b6191b40e409da5
Phan Trinh <pxt177@case.edu>
2019-11-06 00:11:16 -0500

    Adding requirements.txt and append_lyrics

--- append_lyrics.py
+++ append_lyrics.py
@@ -1,13 +1,16 @@
 import lyricsgenius
 import pandas as pd 
+import re
 
-genius = lyricsgenius.Genius() #insert token here
+genius = lyricsgenius.Genius() #insert API token here
 genius.verbose = False
 genius.skip_non_songs = False
 
 data = pd.read_csv('billboard.csv')
+data_file = open('billboard_w_lyrics.csv', 'a')
 
-data.insert(len(data.columns), column='Lyrics', value=["null"]*len(data))
+
+#data.insert(len(data.columns), column='Lyrics', value=["null"]*len(data))
 
 print(data.head())
 
@@ -16,11 +19,11 @@ current_count = 0
 
 for idx, row in data.iterrows():
     song = genius.search_song(row['Title'], row['Artist'])
-    row['Lyrics'] = song.lyrics
+    if song is None:
+        continue
+    row['Lyrics'] = re.sub(r'\[(.*?)\]', '', song.lyrics).replace('\n', ' ').replace('\r', '')
+    data_file.write(','.join(map(str, list(row.values))) + '\n')
     current_count += 1
     if current_count >= num_songs:
         break
-
-data.to_csv()
-
 
